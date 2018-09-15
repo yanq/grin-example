8238321b02b6b7305d36105843872f898c87efe1
yan <37767856@qq.com>
2018-09-15 20:53:18 +0800

    支持了方法方式来定义路由。不过这样，就没有良好的 ide 提示了。

--- grace-app/controllers/Hi.groovy
+++ grace-app/controllers/Hi.groovy
@@ -5,12 +5,45 @@ get('/') {
     response.writer.write("get /")
 }
 
+/**
+ *
+ */
 get('/a') {
     response.writer.write("get /a")
 }
 
+/**
+ *
+ */
 get('/b') {
     response.writer.write("get /b")
 }
+get('/b1') {
+    response.writer.write("get /b")
+}
+get('/b2') {
+    response.writer.write("get /b")
+}
+get('/b3') {
+    response.writer.write("get /b")
+}
+get('/b4') {
+    response.writer.write("get /b")
+}
+get('/b5') {
+    response.writer.write("get /b")
+}
+get('/b6') {
+    response.writer.write("get /b")
+}
+
+"/show" {
+    println("aaaaaaa")
+}
+
+'/eeee'('sss')
+
+
+
 
 new Server().start()
