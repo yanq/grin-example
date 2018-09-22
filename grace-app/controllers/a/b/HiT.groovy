e7822ff0109ae74a7031733dfec456a805dfc0a6
yan <37767856@qq.com>
2018-09-22 17:13:49 +0800

    实现简单控制器产生。

---
+++ grace-app/controllers/a/b/HiT.groovy
@@ -0,0 +1,8 @@
+import static grace.route.Routes.get
+
+/**
+ * 首页
+ */
+get('') {
+    render "你好,Grace !"
+}
--- src/main/groovy/Application.groovy
+++ src/main/groovy/Application.groovy
@@ -1,8 +1,12 @@
+import grace.app.GraceApp
 import grace.app.GraceServer
+import grace.generate.Generator
 
 class Application {
     public static void main(String[] args) {
         new GraceServer().startApp(new File('grace-web'))
         //new GraceServer().startApp()
+//        GraceApp.setRoot(new File('grace-web'))
+//        Generator.controller("a.b.HiT")
     }
 }
