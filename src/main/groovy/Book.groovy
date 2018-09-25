0b91a91f0374dc1669b44f55c20c9cb580d0e649
yan <37767856@qq.com>
2018-09-25 16:11:42 +0800

    初步实现了 get。

--- src/main/groovy/Book.groovy
+++ src/main/groovy/Book.groovy
@@ -1,17 +1,30 @@
+import grace.datastore.DBUtil
 import grace.datastore.Entity
+import groovy.transform.ToString
 import org.grails.datastore.gorm.GormEntity
 
 /**
  * book
  */
-class Book implements Entity<Book>{
+@ToString
+class Book implements Entity<Book> {
     String title
+    Date publishAt
+    static Map mapping = [table: 'book']
+
+    def config = {
+
+    }
 
     public static void main(String[] args) {
-        def book = Book.get(1)
-        Book.declaredFields*.name.each {
-            println("$it")
-        }
+        println Book.get(1)
+//        println(Book.tableName)
+//        println(DBUtil.toDbName("firstName"))
+//        println(DBUtil.toDbName("HTML"))
+        //println(Book.mapping)
+//        Book.declaredFields*.name.each {
+//            println("$it")
+//        }
     }
 }
 
