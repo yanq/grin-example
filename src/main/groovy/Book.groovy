59525d0c8e073093ff5002a1441f79b41d840f72
yan <37767856@qq.com>
2018-09-27 17:10:18 +0800

    实现约束的定义和解析。

--- src/main/groovy/Book.groovy
+++ src/main/groovy/Book.groovy
@@ -1,34 +1,27 @@
 import grace.datastore.DBUtil
 import grace.datastore.Entity
+import grace.datastore.EntityApiImpl
 import groovy.transform.ToString
 import org.grails.datastore.gorm.GormEntity
 
 /**
  * book
  */
-@ToString(includeNames=true)
+@ToString(includeNames = true)
 class Book implements Entity<Book> {
     long id
     String name
     String title
     Date publishAt = new Date()
 
-    static transients = ['config']
-    static mapping = [table: 'book',columns:[name:'title']]
-
-    def config = {
-
+    //static transients = ['config']
+    static mapping = [table: 'book', columns: [name: 'title']]
+    static constraints = {
+        title blank: true, size: 1..5 comment '字符串长度要处于 1 到 5 之间'
     }
 
     public static void main(String[] args) {
-        println Book.get(1)
-//        println(Book.tableName)
-//        println(DBUtil.toDbName("firstName"))
-//        println(DBUtil.toDbName("HTML"))
-        //println(Book.mapping)
-//        Book.declaredFields*.name.each {
-//            println("$it")
-//        }
+       EntityApiImpl.validate(new Book())
     }
 }
 
