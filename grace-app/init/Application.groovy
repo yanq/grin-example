240a69cdba0aad87036f942b2a544f022dc2248b
yan <37767856@qq.com>
2018-09-28 17:19:34 +0800

    梳理环境的设置。 下一步 asset 要区分环境来获取内容。

--- grace-app/init/Application.groovy
+++ grace-app/init/Application.groovy
@@ -10,7 +10,7 @@ class Application {
     public static void main(String[] args) {
         new GraceServer().startApp(new File('grace-web'))
         //new GraceServer().startApp()
-//        GraceApp.setRoot(new File('grace-web'))
+//        GraceApp.setRootAndEnv(new File('grace-web'))
 //        Generator.controller("HiT2")
     }
 }
