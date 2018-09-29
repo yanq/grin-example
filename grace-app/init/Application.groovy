6a48ed854b9acb844be1d4c5563c105a047cf75d
yan <37767856@qq.com>
2018-09-29 17:36:49 +0800

    实现部署模式下 asset 的处理。

--- grace-app/init/Application.groovy
+++ grace-app/init/Application.groovy
@@ -9,6 +9,7 @@ import grace.generate.Generator
 class Application {
     public static void main(String[] args) {
         new GraceServer().startApp(new File('grace-web'))
+        // new GraceServer().startApp(new File('grace-web'),'prod')
         //new GraceServer().startApp()
 //        GraceApp.setRootAndEnv(new File('grace-web'))
 //        Generator.controller("HiT2")
