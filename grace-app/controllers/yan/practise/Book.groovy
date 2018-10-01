d369ec444c4bdb1bb1843a1ec823506b219d5a81
yan <37767856@qq.com>
2018-10-01 17:11:18 +0800

    实现 generate all 命令。从实体类生成控制器和视图。 整体框架搭建起来了，内容，还需进一步的详实。

---
+++ grace-app/controllers/yan/practise/Book.groovy
@@ -0,0 +1,37 @@
+
+package yan.practise
+
+import static grace.controller.route.Routes.*
+
+/**
+ * Book
+ * something about this controller
+ */
+
+get('index') {
+
+}
+
+get('show') {
+
+}
+
+get('create') {
+
+}
+
+post('save') {
+
+}
+
+get('edit') {
+
+}
+
+post('update') {
+
+}
+
+post('delete') {
+
+}
