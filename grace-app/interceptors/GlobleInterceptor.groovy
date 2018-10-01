cdef9e883c6da3140f918c078046eca94c359591
yan <37767856@qq.com>
2018-10-01 17:39:59 +0800

    默认控制器增加了 Controller 后缀，因为生成的时候，和实体类重名了。

--- grace-app/interceptors/GlobleInterceptor.groovy
+++ grace-app/interceptors/GlobleInterceptor.groovy
@@ -2,6 +2,6 @@ import grace.controller.route.Interceptor
 
 import static grace.controller.route.Routes.before
 
-before('/**',Interceptor.ORDER_HIGH) {
-    // log.info("request : $request.requestURI")
+before('',Interceptor.ORDER_HIGH) {
+    log.info("request : $request.requestURI")
 }
