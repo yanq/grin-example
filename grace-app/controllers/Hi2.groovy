37c38f6b201ab38424a139844721496481718b48
yanq <yqp_001@163.com>
2018-10-05 18:53:28 +0800

    实现 list 页面的模板。

--- grace-app/controllers/Hi2.groovy
+++ grace-app/controllers/Hi2.groovy
@@ -1,7 +1,20 @@
-import grace.generate.FormGenerator
-import grace.util.ClassUtil
+import grace.generate.Generator
 import yan.practise.Book
 
-def r = FormGenerator.generateForm(Book)
-println r
+def content = Generator.generate(new File('D:\\IdeaProjects\\grace-dev\\grace-web\\grace-app\\templates\\index.html'), [entityClass: Book])
+println content
 
+def entityClass = Book
+def out = []
+
+int i = 0
+entityClass.props.each {
+    if (i == 0) {
+        def p = "${grace.util.ClassUtil.propertyName(entityClass)}.${it}"
+        out << "<td><a href=\"#\" th:text=\"\${${p}}\" th:href=\"|show/\${${p}}|\">PropertyValue</a></td> \n"
+    } else {
+        out << " <td th:text=\"\${${p}\">PropertyValue</td> \n "
+    }
+}
+
+println(out)
