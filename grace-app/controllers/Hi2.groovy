1c130f8132ca480bfa72fa8f1d4a0d82f5e4702d
yanq <yqp_001@163.com>
2018-10-06 09:54:39 +0800

    create 页面，大概生成。

--- grace-app/controllers/Hi2.groovy
+++ grace-app/controllers/Hi2.groovy
@@ -1,20 +1,12 @@
+import grace.app.GraceApp
 import grace.generate.Generator
 import yan.practise.Book
 
-//def content = Generator.generate(new File('D:\\IdeaProjects\\grace-dev\\grace-web\\grace-app\\templates\\index.html'), [entityClass: Book])
-//println content
-//
-//def entityClass = Book
-//def out = []
-//
-//int i = 0
-//entityClass.props.each {
-//    if (i == 0) {
-//        def p = "${grace.util.ClassUtil.propertyName(entityClass)}.${it}"
-//        out << "<td><a href=\"#\" th:text=\"\${${p}}\" th:href=\"|show/\${${p}}|\">PropertyValue</a></td> \n"
-//    } else {
-//        out << " <td th:text=\"\${${p}\">PropertyValue</td> \n "
-//    }
-//}
-//
-//println(out)
+def entityClass = Book
+def out = []
+
+GraceApp.setRootAndEnv(new File('D:\\IdeaProjects\\grace-dev\\grace-web'))
+
+def content = Generator.generate(new File('D:\\IdeaProjects\\grace-dev\\grace-web\\grace-app\\templates\\create.html'), [entityClass: entityClass])
+println content
+
