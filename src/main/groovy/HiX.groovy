98cdeb9ef6bd00469048ef87ff0e9cb6fd234bf3
yanq <yqp_001@163.com>
2018-10-12 10:37:40 +0800

    db datasource 可配置化，这样，就可以外部使用 entity 了。

---
+++ src/main/groovy/HiX.groovy
@@ -0,0 +1,13 @@
+import grace.datastore.entity.Entity
+import yan.practise.Book
+
+/**
+ * 测试用
+ */
+class HiX {
+    public static void main(String[] args) {
+        Book book = new Book()
+        Book.get(1)
+        println book instanceof Entity
+    }
+}
