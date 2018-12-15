79707973b0dfe324655d0960167b42677734d26c
Yan <yqp_001@163.com>
2018-12-15 12:40:44 +0800

    重构 db util 为 entity util。这里更合适一点。

--- grace-app/scripts/HiGrace.groovy
+++ grace-app/scripts/HiGrace.groovy
@@ -1,5 +1,5 @@
 import grace.app.GraceApp
-import grace.datastore.DBUtil
+import grace.datastore.entity.EntityUtil
 import yan.practise.Author
 import yan.practise.Book
 
@@ -10,7 +10,7 @@ import yan.practise.Book
 GraceApp.setRootAndEnv(new File('D:\\IdeaProjects\\grace-dev\\grace-example'))
 
 def books = Book.where('id in (?,?,?)',1,2,3).list()
-DBUtil.fetch(books)
+EntityUtil.fetch(books)
 books.each {
     println(it)
     it.author = new Author(id: 2)
