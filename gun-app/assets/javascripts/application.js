7d618120b9ab3ddb9d98e55d2edaa87b27d035d3
yan <yqp_001@163.com>
2022-01-25 17:49:01 +0800

    更新 bs 5.1.3

--- gun-app/assets/javascripts/application.js
+++ gun-app/assets/javascripts/application.js
@@ -1,16 +1,9 @@
 // This is a manifest file that'll be compiled into application.js.
-//= encoding UTF-8
-//= require jquery-3.3.1
+//
+// Any JavaScript file within this directory can be referenced here using a relative path.
+//
+// You're free to add application-wide JavaScript to this file, but it's generally better
+// to create separate JavaScript files as needed.
+//
 //= require bootstrap.bundle
-//= require_tree .
 //= require_self
-
-if (typeof jQuery !== 'undefined') {
-    (function($) {
-        $(document).ajaxStart(function() {
-            $('#spinner').fadeIn();
-        }).ajaxStop(function() {
-            $('#spinner').fadeOut();
-        });
-    })(jQuery);
-}
