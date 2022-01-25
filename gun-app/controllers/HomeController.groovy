129174ea6643b425514a0bf7e0c5a208a61267cc
yan <yqp_001@163.com>
2022-01-25 19:07:53 +0800

    完善，模板基本可用。

--- gun-app/controllers/HomeController.groovy
+++ gun-app/controllers/HomeController.groovy
@@ -1,8 +1,10 @@
 import gun.web.Controller
 
-class HomeController extends Controller{
+class HomeController extends Controller {
+    def a = '1234'
 
-    def index(){
-        render('Hello Gun !')
+    def index() {
+        render('/index', [:])
+        // render("abcde")
     }
 }
