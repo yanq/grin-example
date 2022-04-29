306d0e8292ece6fc94d5f0ca3dbc84f518d5eee8
yan <yqp_001@163.com>
2022-04-29 14:53:27 +0800

    Server 默认值处理改善。

--- gun-app/conf/config.groovy
+++ gun-app/conf/config.groovy
@@ -1,15 +1,31 @@
 // config
 
-// 文件上传
-fileUpload.location = '/temp/uploads'
+// 服务器
+// port 设置 -1 表示不启用
+server {
+    host = 'localhost'
+    http.port = 8080
+    // https {
+    //     port = 8443
+    //     jksPath = ''
+    //     jksPwd = ''
+    // }
+    // context = '/'
+    // uploadLocation = '/Volumes/data/dev/uploads'
+    // maxFileSize = -1L
+    // maxRequestSize = -1L
+    // fileSizeThreshold = 0
+    // ioThreads = 2
+    // workerThreads = 5
+}
+
+serverURL = "http://localhost:8080"
+
 // 处理错误的控制器类
 // errorClass = Controller.class
 // views
 views.notFound = '/notFound.html'
 views.error = '/error.html'
-
-serverURL = "http://localhost:8080"
-fileUpload.location = '/Volumes/data/dev/uploads'
 
 // environments
 environments {
@@ -28,11 +44,11 @@ environments {
             removeAbandoned = true
             removeAbandonedTimeout = 10000
         }
-         logSql = true
+        logSql = true
     }
     prod {
         name = "Gun Prod"
     }
 }
 
-json.dateFormat = 'yyyy'
+json.dateFormat = 'yyyy-MM-dd HH:mm:ss'
