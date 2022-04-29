ee60fa8b319077fca22e73fb09919ff9456c4f4f
yan <yqp_001@163.com>
2022-04-29 15:53:57 +0800

    完善项目说明。

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ A project used Gun.
 # Features
 
 Like Grails.
+
+# 开发与部署
+
+开发环境，使用 gradle run 来运行调试。
+
+部署环境，用 tools 来运行和管理。
--- gun-app/init/Application.groovy
+++ gun-app/init/Application.groovy
@@ -3,13 +3,12 @@ import gun.app.GunApp
 import gun.app.GunServer
 
 /**
- * 启动
- * 此类只是方便开发调试使用。不作为正式运行环境启动入口。
+ * 启动 APP
  */
 @Slf4j
 class Application {
     public static void main(String[] args) {
-        // GunApp.init() // 这里是为了初始化一下，如果不的话，会在第一次用到这个的时候初始化
+        // GunApp.init() // 需要配置特定路径和环境的时候启用
         GunApp.instance.startServer(new GunServer())
     }
 }
