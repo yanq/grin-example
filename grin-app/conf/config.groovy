25facdde6ef0251e47345411d1e12080ae7538cc
yan <yqp_001@163.com>
2022-11-16 15:19:51 +0800

    整理 config 文件，去掉不再使用的配置项。

--- grin-app/conf/config.groovy
+++ grin-app/conf/config.groovy
@@ -10,34 +10,14 @@ urlMapping = [
         '/@controllerName/?@actionName?/?@id?': '',
 ]
 
-webSocketEntryList = [WebSocketEntry]
-
-// 服务器
-// port 设置 -1 表示不启用
-server {
-    host = 'localhost'
-    http.port = 8080
-    // https {
-    //     port = 8443
-    //     jksPath = ''
-    //     jksPwd = ''
-    // }
-    // context = '/'
-    // uploadLocation = '/Volumes/data/dev/uploads'
-    // maxFileSize = -1L
-    // maxRequestSize = -1L
-    // fileSizeThreshold = 0
-    // ioThreads = 2
-    // workerThreads = 5
-}
-
-serverURL = "http://localhost:8080"
-
 // 处理错误的控制器类（可选，默认用 Controller）
 // errorClass = Controller.class
 // views
 views.notFound = '/notFound.html'
 views.error = '/error.html'
+
+// 服务器 url
+serverURL = "http://localhost:8080"
 
 // environments
 environments {
